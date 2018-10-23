831157cec6c8fffd0b59e77a945bbe416cc4e7a6
saynschuman <saynschuman@gmail.com>
2018-10-23 19:07:41 +0300

    loginHeader

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import AuthForm from "./components/AuthForm/AuthForm";
+import LoginHeader from "./components/commmon/LoginHeader/LoginHeader";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div>
+        <LoginHeader />
         <AuthForm />
       </div>
     );
