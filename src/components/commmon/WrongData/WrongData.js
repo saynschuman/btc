6295797d35e404a7a159a41089e56c535de123b9
saynschuman <saynschuman@gmail.com>
2018-10-24 15:46:58 +0300

    close

--- src/components/commmon/WrongData/WrongData.js
+++ src/components/commmon/WrongData/WrongData.js
@@ -2,9 +2,15 @@ import React from "react";
 import "./WrongData.scss";
 
 export default () => {
+  const exit = e => {
+    e.preventDefault();
+    localStorage.clear();
+    window.location.reload();
+  };
   return (
     <div className="wrongData">
       <div>Неверный ID или пароль</div>
+      <a onClick={exit} className="closeWrong" href="/" />
     </div>
   );
 };
