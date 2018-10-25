72f6f086b4efcd9cd79be6840f2ac38920facdc7
saynschuman <saynschuman@gmail.com>
2018-10-25 16:53:59 +0300

    graf

--- src/components/admin/AdminHomepage/AdminHomepage.js
+++ src/components/admin/AdminHomepage/AdminHomepage.js
@@ -1,5 +1,33 @@
 import React from "react";
 import "./AdminHomepage.scss";
+import {Line} from 'react-chartjs-2';
+
+const data = {
+    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+    datasets: [
+        {
+            label: 'My First dataset',
+            fill: false,
+            lineTension: 0.1,
+            backgroundColor: 'rgba(75,192,192,0.4)',
+            borderColor: 'rgba(75,192,192,1)',
+            borderCapStyle: 'butt',
+            borderDash: [],
+            borderDashOffset: 0.0,
+            borderJoinStyle: 'miter',
+            pointBorderColor: 'rgba(75,192,192,1)',
+            pointBackgroundColor: '#fff',
+            pointBorderWidth: 1,
+            pointHoverRadius: 5,
+            pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+            pointHoverBorderColor: 'rgba(220,220,220,1)',
+            pointHoverBorderWidth: 2,
+            pointRadius: 1,
+            pointHitRadius: 10,
+            data: [65, 59, 80, 81, 56, 55, 40]
+        }
+    ]
+};
 
 class AdminHomepage extends React.Component {
   render() {
@@ -36,7 +64,7 @@ class AdminHomepage extends React.Component {
                     <div className="block-header">
                       <div className="header-title">График курса BTC</div>
                     </div>
-                    <div className="gr"/>
+                    <Line data={data} />
                     <p className="block-body smaller28">1BTC = 6 952,04$      1BTC = 399 574P</p>
                 </div>
               <div className="col middle mr">
