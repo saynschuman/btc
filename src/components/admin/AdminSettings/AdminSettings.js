ba25c6b3b2a25cb354b2275d5f8d6d24fa144ba1
saynschuman <saynschuman@gmail.com>
2018-10-26 16:50:18 +0300

    scrollbar table

--- src/components/admin/AdminSettings/AdminSettings.js
+++ src/components/admin/AdminSettings/AdminSettings.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./AdminSettings.scss";
 import Select from "react-select";
+import PerfectScrollbar from "react-perfect-scrollbar";
 
 const options = [
   { value: "chocolate", label: "Chocolate" },
@@ -10,7 +11,7 @@ const options = [
 
 class AdminSettings extends React.Component {
   state = {
-    selectedOption: null,
+    selectedOption: "",
     password: "1234567890",
     email: "watson@gmail.com",
     id: "412412532               "
@@ -29,59 +30,224 @@ class AdminSettings extends React.Component {
         <div className="admin-block">
           <div className="settings-header">Администраторы</div>
           <div className="settings-body">
-            <table className={"settings-table"}>
-              <tbody>
-                <tr>
-                  <th>ID</th>
-                  <th>Пароль</th>
-                  <th>Права</th>
-                  <th>E-mail</th>
-                  <th>Действие</th>
-                  <th />
-                </tr>
-                <tr>
-                  <td>
-                    <input
-                      className={"id"}
-                      type="text"
-                      onChange={e => this.handleChange(e, "id")}
-                      value={this.state.id}
-                    />
-                  </td>
-                  <td>
-                    <input
-                      type="password"
-                      className={"password"}
-                      onChange={e => this.handleChange(e, "password")}
-                      value={this.state.password}
-                    />
-                  </td>
-                  <td>
-                    <Select
-                      className={"rules"}
-                      value={this.state.selectedOption}
-                      onChange={this.handleChangeOption}
-                      options={options}
-                    />
-                  </td>
-                  <td>
-                    <input
-                      type="text"
-                      value={this.state.email}
-                      onChange={e => this.handleChange(e, "email")}
-                    />
-                  </td>
-                  <td>
-                    <button>Редактировать</button>
-                  </td>
-                  <td>
-                    <div className="close-row" />
-                  </td>
-                </tr>
-              </tbody>
-            </table>
+            <PerfectScrollbar>
+              <table className={"settings-table"}>
+                <tbody>
+                  <tr>
+                    <th>ID</th>
+                    <th>Пароль</th>
+                    <th>Права</th>
+                    <th>E-mail</th>
+                    <th>Действие</th>
+                    <th />
+                  </tr>
+                  <tr>
+                    <td>
+                      <input
+                        className={"id"}
+                        type="text"
+                        onChange={e => this.handleChange(e, "id")}
+                        value={this.state.id}
+                      />
+                    </td>
+                    <td>
+                      <input
+                        type="password"
+                        className={"password"}
+                        onChange={e => this.handleChange(e, "password")}
+                        value={this.state.password}
+                      />
+                    </td>
+                    <td>
+                      <Select
+                        className={"rules"}
+                        value={this.state.selectedOption}
+                        onChange={this.handleChangeOption}
+                        options={options}
+                        placeholder={"Выберите права"}
+                      />
+                    </td>
+                    <td>
+                      <input
+                        type="text"
+                        value={this.state.email}
+                        onChange={e => this.handleChange(e, "email")}
+                      />
+                    </td>
+                    <td>
+                      <button>Редактировать</button>
+                    </td>
+                    <td>
+                      <div className="close-row" />
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <input
+                        className={"id"}
+                        type="text"
+                        onChange={e => this.handleChange(e, "id")}
+                        value={this.state.id}
+                      />
+                    </td>
+                    <td>
+                      <input
+                        type="password"
+                        className={"password"}
+                        onChange={e => this.handleChange(e, "password")}
+                        value={this.state.password}
+                      />
+                    </td>
+                    <td>
+                      <Select
+                        className={"rules"}
+                        value={this.state.selectedOption}
+                        onChange={this.handleChangeOption}
+                        options={options}
+                        placeholder={"Выберите права"}
+                      />
+                    </td>
+                    <td>
+                      <input
+                        type="text"
+                        value={this.state.email}
+                        onChange={e => this.handleChange(e, "email")}
+                      />
+                    </td>
+                    <td>
+                      <button>Редактировать</button>
+                    </td>
+                    <td>
+                      <div className="close-row" />
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <input
+                        className={"id"}
+                        type="text"
+                        onChange={e => this.handleChange(e, "id")}
+                        value={this.state.id}
+                      />
+                    </td>
+                    <td>
+                      <input
+                        type="password"
+                        className={"password"}
+                        onChange={e => this.handleChange(e, "password")}
+                        value={this.state.password}
+                      />
+                    </td>
+                    <td>
+                      <Select
+                        className={"rules"}
+                        value={this.state.selectedOption}
+                        onChange={this.handleChangeOption}
+                        options={options}
+                        placeholder={"Выберите права"}
+                      />
+                    </td>
+                    <td>
+                      <input
+                        type="text"
+                        value={this.state.email}
+                        onChange={e => this.handleChange(e, "email")}
+                      />
+                    </td>
+                    <td>
+                      <button>Редактировать</button>
+                    </td>
+                    <td>
+                      <div className="close-row" />
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <input
+                        className={"id"}
+                        type="text"
+                        onChange={e => this.handleChange(e, "id")}
+                        value={this.state.id}
+                      />
+                    </td>
+                    <td>
+                      <input
+                        type="password"
+                        className={"password"}
+                        onChange={e => this.handleChange(e, "password")}
+                        value={this.state.password}
+                      />
+                    </td>
+                    <td>
+                      <Select
+                        className={"rules"}
+                        value={this.state.selectedOption}
+                        onChange={this.handleChangeOption}
+                        options={options}
+                        placeholder={"Выберите права"}
+                      />
+                    </td>
+                    <td>
+                      <input
+                        type="text"
+                        value={this.state.email}
+                        onChange={e => this.handleChange(e, "email")}
+                      />
+                    </td>
+                    <td>
+                      <button>Редактировать</button>
+                    </td>
+                    <td>
+                      <div className="close-row" />
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <input
+                        className={"id"}
+                        type="text"
+                        onChange={e => this.handleChange(e, "id")}
+                        value={this.state.id}
+                      />
+                    </td>
+                    <td>
+                      <input
+                        type="password"
+                        className={"password"}
+                        onChange={e => this.handleChange(e, "password")}
+                        value={this.state.password}
+                      />
+                    </td>
+                    <td>
+                      <Select
+                        className={"rules"}
+                        value={this.state.selectedOption}
+                        onChange={this.handleChangeOption}
+                        options={options}
+                        placeholder={"Выберите права"}
+                      />
+                    </td>
+                    <td>
+                      <input
+                        type="text"
+                        value={this.state.email}
+                        onChange={e => this.handleChange(e, "email")}
+                      />
+                    </td>
+                    <td>
+                      <button>Редактировать</button>
+                    </td>
+                    <td>
+                      <div className="close-row" />
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </PerfectScrollbar>
           </div>
-          <div className="settings-footer">footer</div>
+          <div className="settings-footer">
+            <button className={"settings-save"}>Сохранить</button>
+          </div>
         </div>
       </div>
     );
