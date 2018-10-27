4d8397cb4be6e3b5fe763926e71688875e62f577
saynschuman <saynschuman@gmail.com>
2018-10-27 11:21:43 +0300

    select fixed

--- src/components/admin/AdminSettings/AdminSettings.js
+++ src/components/admin/AdminSettings/AdminSettings.js
@@ -1,28 +1,24 @@
 import React from "react";
 import "./AdminSettings.scss";
-import Select from "react-select";
 import PerfectScrollbar from "react-perfect-scrollbar";
+import CustomSelect from "../../commmon/CustomSelect/CustomSelect";
 
-const options = [
-  { value: "chocolate", label: "Chocolate" },
-  { value: "strawberry", label: "Strawberry" },
-  { value: "vanilla", label: "Vanilla" }
+const options1 = [
+  { value: "admin", label: "Админ" },
+  { value: "moderator", label: "Модератор" },
+  { value: "investor", label: "Инвестор" }
 ];
 
 class AdminSettings extends React.Component {
   state = {
-    selectedOption: "",
     password: "1234567890",
     email: "watson@gmail.com",
-    id: "412412532               "
+    id: "412412532"
   };
   handleChange = (e, key) => {
     this.setState({
       [key]: e.target.value
     });
-  };
-  handleChangeOption = selectedOption => {
-    this.setState({ selectedOption });
   };
   render() {
     return (
@@ -59,13 +55,7 @@ class AdminSettings extends React.Component {
                       />
                     </td>
                     <td>
-                      <Select
-                        className={"rules"}
-                        value={this.state.selectedOption}
-                        onChange={this.handleChangeOption}
-                        options={options}
-                        placeholder={"Выберите права"}
-                      />
+                      <CustomSelect data={options1} />
                     </td>
                     <td>
                       <input
@@ -99,13 +89,7 @@ class AdminSettings extends React.Component {
                       />
                     </td>
                     <td>
-                      <Select
-                        className={"rules"}
-                        value={this.state.selectedOption}
-                        onChange={this.handleChangeOption}
-                        options={options}
-                        placeholder={"Выберите права"}
-                      />
+                      <CustomSelect data={options1} />
                     </td>
                     <td>
                       <input
@@ -139,13 +123,7 @@ class AdminSettings extends React.Component {
                       />
                     </td>
                     <td>
-                      <Select
-                        className={"rules"}
-                        value={this.state.selectedOption}
-                        onChange={this.handleChangeOption}
-                        options={options}
-                        placeholder={"Выберите права"}
-                      />
+                      <CustomSelect data={options1} />
                     </td>
                     <td>
                       <input
@@ -179,13 +157,7 @@ class AdminSettings extends React.Component {
                       />
                     </td>
                     <td>
-                      <Select
-                        className={"rules"}
-                        value={this.state.selectedOption}
-                        onChange={this.handleChangeOption}
-                        options={options}
-                        placeholder={"Выберите права"}
-                      />
+                      <CustomSelect data={options1} />
                     </td>
                     <td>
                       <input
@@ -219,13 +191,41 @@ class AdminSettings extends React.Component {
                       />
                     </td>
                     <td>
-                      <Select
-                        className={"rules"}
-                        value={this.state.selectedOption}
-                        onChange={this.handleChangeOption}
-                        options={options}
-                        placeholder={"Выберите права"}
+                      <CustomSelect data={options1} />
+                    </td>
+                    <td>
+                      <input
+                        type="text"
+                        value={this.state.email}
+                        onChange={e => this.handleChange(e, "email")}
                       />
+                    </td>
+                    <td>
+                      <button>Редактировать</button>
+                    </td>
+                    <td>
+                      <div className="close-row" />
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <input
+                        className={"id"}
+                        type="text"
+                        onChange={e => this.handleChange(e, "id")}
+                        value={this.state.id}
+                      />
+                    </td>
+                    <td>
+                      <input
+                        type="password"
+                        className={"password"}
+                        onChange={e => this.handleChange(e, "password")}
+                        value={this.state.password}
+                      />
+                    </td>
+                    <td>
+                      <CustomSelect data={options1} />
                     </td>
                     <td>
                       <input
