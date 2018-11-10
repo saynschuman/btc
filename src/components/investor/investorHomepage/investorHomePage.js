92b37d91cc50e40d1f77d3ef64f8f67c75313cb6
saynschuman <saynschuman@gmail.com>
2018-11-10 11:47:07 +0200

    one block finished

--- src/components/investor/investorHomepage/investorHomePage.js
+++ src/components/investor/investorHomepage/investorHomePage.js
@@ -2,8 +2,12 @@ import React from 'react'
 import Header from '../../commmon/Header/Header'
 import Main from '../common/main/main'
 import Block from '../common/block'
+import BlockNom from '../common/blockNom'
 import Aside from '../common/aside/aside'
 import MainContainer from '../common/MainContainer/MainContainer'
+import { flash, btc, wallet, course } from './img/export'
+import Numbers from '../common/Numbers/Numbers'
+import ButtonViolet from '../common/ButtonViolet/ButtonViolet'
 
 const InvestorHomepage = () => {
   return (
@@ -11,10 +15,22 @@ const InvestorHomepage = () => {
       {/* <Header whoAreYou={'investor'} /> */}
       <MainContainer>
         <Aside>
-          <Block>123</Block>
+          <Block title={'Ваша мощность (?)'} icon={flash}>
+            <Numbers data={'1.201 TH/s'} />
+          </Block>
+          <BlockNom title={'Полученный доход (?)'} icon={btc}>
+            <Numbers data={'1.201 BTC'} />
+            <ButtonViolet info text={'Инвестировать'} />
+          </BlockNom>
+          <Block title={'Баланс кошелька'} icon={wallet}>
+            123
+          </Block>
+          <Block title={'Курс'} icon={course}>
+            123
+          </Block>
         </Aside>
         <Main>
-          <Block>123</Block>
+          <Block title={'График курса'}>123</Block>
         </Main>
       </MainContainer>
     </div>
