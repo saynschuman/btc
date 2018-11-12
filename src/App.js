a5de3558adaba3596f9404ef9412a795b0dd0198
saynschuman <saynschuman@gmail.com>
2018-11-12 13:17:17 +0200

    router added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,39 @@
 import React, { Component } from 'react'
 import './App.css'
-import { authenticate } from './helpers/authenticate'
+// import { authenticate } from './helpers/authenticate'
+import { Router, Link } from '@reach/router'
 import { parseJwt } from './helpers/parseJwt'
 import RenderInterface from './components/commmon/RenderInterface/RenderInterface'
 
 class App extends Component {
-  componentDidMount() {
-    window.location.hash.length > 30 && authenticate(window.location.hash)
-    console.log('home')
-  }
+  // componentDidMount() {
+  //   window.location.hash.length > 30 && authenticate(window.location.hash)
+  // }
   render() {
     const userInfo = parseJwt(localStorage.getItem('token'))
-    return <div>{<RenderInterface userinfo={userInfo} />}</div>
+    // return <div>{<RenderInterface userinfo={userInfo} />}</div>
+    return (
+      <Router>
+        <AdminLogin path="/admin-login">
+          <AdminHomepage exact path="/" />
+          <AdminSettings path="/settings" />
+          <AdminReports path="/reports" />
+          <AdminPays path="/pays" />
+          <Applications path="/applications" />
+          <AdminSchema path="/schema" />
+          <AdminOther path="/other" />
+          <News path="/news" />
+          <AdminHomepage path="/homepage" />
+          <AdminHomepage default />
+        </AdminLogin>
+        <Login path="/login">
+          <InvestorHomePage exact path="/investor-home" />
+          <InvestorSettings path="/investor-buy" />
+          <InvestorPrivateSettings path="/investor-settings" />
+          <InvestorHomePage default />
+        </Login>
+      </Router>
+    )
   }
 }
 
